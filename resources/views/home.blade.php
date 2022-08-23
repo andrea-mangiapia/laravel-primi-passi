f9268b0eedc7cfca2e3fa37ccd0b421fc9ac592f
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-08-23 14:19:42 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+</head>
+<body>
+    <h1>Hello World</h1>
+
+    <p>
+        Introduzione a Laravel
+    </p>
+    
+</body>
+</html>
